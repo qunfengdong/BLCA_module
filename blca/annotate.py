0870d651bef5534242950191acf6315dd7bf39ee
Kashi Revanna <kashi.mail@gmail.com>
2016-05-05 15:03:24 -0500

    example folder

--- blca/annotate.py
+++ blca/annotate.py
@@ -134,7 +134,7 @@ def annotate():
 
 	outfile = open(my_module.OUTFILE, "w")
 	#print("\t" + "\t".join(reversed(order)))
-	outfile.write("\t" + "\t".join(reversed(order)) + "\n")
+	outfile.write("sequence_id\t" + "\t".join(reversed(order)) + "\n")
 	#ann = read_16s_annotation(read_names, read_nodes)
 	#megan = read_megan_output(filename)
 	#rdp = read_rdp_output(filename)
